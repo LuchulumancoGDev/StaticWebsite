c3cde06a9ffae86d83f81e26c5b456b80820ca8e
LuchulumancoGDev <131538691+LuchulumancoGDev@users.noreply.github.com>
2025-05-31 13:56:37 +0200

    updates

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 // config.js
+require('dotenv').config();
+
 module.exports = {
     service: 'gmail',
     auth: {
-      user: 'manabana.electrical@gmail.com',
-      pass: 'kpzkupaxlpdbbzks',
+      user: process.env.EMAIL_USER || '',
+      pass: process.env.EMAIL_PASS || '',
     },
   };
   
